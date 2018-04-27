93e7aa29dfb4d4e12520cdb873524ffabac0afaa
bokuweb <bokuweb12@gmail.com>
2018-04-27 17:00:11 +0900

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# vscode-ripgrep
+
+## Features
+
+ripgrep extension for Visual Studio Code.
+
+![screenshot](https://github.com/bokuweb/vscode-ripgrep/blob/master/docs/screenshot.gif?raw=true)
+
+## Extension Settings
+
+## Known Issues
+
+## Release Notes
+
+### 0.1.0
+
+- initial
---
+++ src/extension.ts
@@ -0,0 +1,83 @@
+"use strict";
+
+import { exec } from "child_process";
+import { quote } from "shell-quote";
+import {
+  window,
+  commands,
+  ExtensionContext,
+  workspace,
+  Selection,
+  QuickPickItem,
+  QuickPickOptions
+} from "vscode";
+
+interface QuickPickItemWithPath extends QuickPickItem {
+  fullPath: string;
+}
+
+const projectRoot = workspace.rootPath ? workspace.rootPath : ".";
+
+export function activate(context: ExtensionContext) {
+  (async () => {
+    const disposable = commands.registerCommand(
+      "extension.ripGrep",
+      async () => {
+        const query = await window.showInputBox({
+          prompt: "Please input search word."
+        });
+        const command = quote([
+          require("vscode-ripgrep").rgPath,
+          "-n",
+          ...query.split(" ")
+        ]);
+        const fetchItems = (): Promise<QuickPickItemWithPath[]> =>
+          new Promise((resolve, reject) => {
+            exec(
+              command,
+              { cwd: projectRoot, maxBuffer: 200000 * 1024 },
+              (err, stdout, stderr) => {
+                if (stderr) {
+                  window.showErrorMessage(stderr);
+                  return resolve([]);
+                }
+                const lines = stdout
+                  .split(/\n/)
+                  .filter(l => l !== "")
+                  .map(l => l.trim());
+                if (!lines.length) {
+                  window.showInformationMessage("There are no items.");
+                  return resolve([]);
+                }
+                return resolve(
+                  lines.map(l => {
+                    const [fullPath, line, ...desc] = l.split(":");
+                    const path = fullPath.split("/");
+                    return {
+                      label: `${path[path.length - 1]} : ${line}`,
+                      description: desc.join(":").trim(),
+                      detail: fullPath,
+                      fullPath: l
+                    };
+                  })
+                );
+              }
+            );
+          });
+        const options: QuickPickOptions = { matchOnDescription: true };
+        const item = await window.showQuickPick(fetchItems(), options);
+        if (!item) return;
+        const [file, line] = item.fullPath.split(":");
+        const doc = await workspace.openTextDocument(projectRoot + "/" + file);
+        await window.showTextDocument(doc);
+        window.activeTextEditor.selection = new Selection(~~line, 0, ~~line, 0);
+        commands.executeCommand("cursorUp");
+        context.subscriptions.push(disposable);
+      }
+    );
+  })().catch(error => {
+    window.showErrorMessage(error);
+  });
+}
+
+export function deactivate() {}
